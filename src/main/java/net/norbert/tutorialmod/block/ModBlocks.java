b5fcab5f46e6fbdfa226783a6fcd52629fb856dd
Bomby888 <norbertkolud@gmail.com>
2023-11-16 11:13:53 +0100

    added loot tables for mogus_block, mogus_ore_block and raw_mogus_block

--- src/main/java/net/norbert/tutorialmod/block/ModBlocks.java
+++ src/main/java/net/norbert/tutorialmod/block/ModBlocks.java
@@ -4,12 +4,14 @@ import net.fabricmc.fabric.api.item.v1.FabricItemSettings;
 import net.fabricmc.fabric.api.object.builder.v1.block.FabricBlockSettings;
 import net.minecraft.block.Block;
 import net.minecraft.block.Blocks;
+import net.minecraft.block.ExperienceDroppingBlock;
 import net.minecraft.item.BlockItem;
 import net.minecraft.item.Item;
 import net.minecraft.registry.Registries;
 import net.minecraft.registry.Registry;
 import net.minecraft.sound.BlockSoundGroup;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.intprovider.UniformIntProvider;
 import net.norbert.tutorialmod.TutorialMod;
 
 public class ModBlocks {
@@ -20,8 +22,9 @@ public class ModBlocks {
             ,new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.SLIME)));
     //FabricBlockSettings.create()
 
-    public static final Block MOGUS_ORE_BLOCK = registerBlock("mogus_ore_block"
-            ,new Block(FabricBlockSettings.copyOf(Blocks.IRON_BLOCK).sounds(BlockSoundGroup.HONEY)));
+    public static final Block MOGUS_ORE_BLOCK =registerBlock("mogus_ore_block",
+            new ExperienceDroppingBlock(FabricBlockSettings.copyOf(Blocks.NETHERITE_BLOCK).sounds(BlockSoundGroup.HONEY).strength(2f), UniformIntProvider.create(2,5)));
+
     private static Block registerBlock(String name, Block block){
         registerBlockItem(name,block);
         return Registry.register(Registries.BLOCK, new Identifier(TutorialMod.MOD_ID,name),
